d7a052df4e3ffb42c50072a500b6f3b9f313fdb2
Khoa Doan <kdoan1991@gmail.com>
2022-04-09 02:37:34 -0700

    Update Min Cost Climbing Stairs

--- neetcode/9.1-D-dynamic-programming/minCostClimbingStairs.py
+++ neetcode/9.1-D-dynamic-programming/minCostClimbingStairs.py
@@ -1,5 +1,6 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+        """
         # Reaching the top of the stair requires (len(cost) + 1) steps
         # The cost to reach the 0th and 1st step is 0
         minCost = [0] * (len(cost) + 1)
@@ -8,3 +9,12 @@ class Solution:
             minCost[i] = min(minCost[i-2] + cost[i-2], minCost[i-1] + cost[i-1])
             
         return minCost[-1]
+        """
+
+        # Dynamic programming backwards
+        cost.append(0)
+        
+        for i in range(len(cost) - 3, -1, -1):
+            cost[i] += min(cost[i + 1], cost[i + 2])
+            
+        return min(cost[0], cost[1])
